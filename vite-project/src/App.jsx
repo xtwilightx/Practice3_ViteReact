15ec971c9a6a4cd0abfff8d9c8be8e655e0cc1ee
xtwilightx <iqxidi@gmail.com>
2025-07-15 17:59:31 +0300

    testBranch2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,19 +5,42 @@ import './App.css'
 import Welcome from './Components/Welcome'
 import { useEffect } from 'react'
 
-// function Profile() {
-//   return(
-//     <div className='size-24 bg-amber-500'></div>
-//   );
-// }
-const [error, setError] = useState(null);
-const [Loaded, setIsLoaded] = useState(false);
-const [items, setItems] = useState([]);
 
 function App() {
-
-      fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+const [error, setError] = useState(null);
+const [isLoaded, setIsLoaded] = useState(false);
+const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
       .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.drinks);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+    if (error) {
+      return <p>Error {error.message}</p>
+    }
+    else if(!isLoaded){
+      return <p> Loading... </p>
+    }
+    else{
+      return (
+        <ul>
+          {items.map(item => (
+            <li key = {item.name}>{item.strDrink}</li>  
+          ))}
+        </ul>
+      )
+    }
+
   return (
     <div>
 
